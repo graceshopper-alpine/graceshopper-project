a02c02483b73c2d9ac7bd294d9689034da270a0f
vhuang325 <vhuang325@gmail.com>
2023-07-19 17:31:53 -0400

    finished backend api route for single product

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,21 @@
-const router = require('express').Router()
-module.exports = router
+const router = require("express").Router();
 
-router.use('/users', require('./users'))
+router.use("/users", require("./users"));
 
 router.use((req, res, next) => {
-  const error = new Error('Not Found')
-  error.status = 404
-  next(error)
-})
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+// view single product
+router.get("/products/:id", async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.id);
+    res.json(product);
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
