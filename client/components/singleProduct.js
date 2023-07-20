3c6b8345854b2d6295b767060712c62c46430af7
vhuang325 <vhuang325@gmail.com>
2023-07-20 17:17:08 -0400

    added some styling to single product could do more later

--- client/components/singleProduct.js
+++ client/components/singleProduct.js
@@ -5,46 +5,59 @@ import { fetchSingleProduct } from "../store/singleProductSlice";
 import axios from "axios";
 
 const SingleProduct = () => {
-    const dispatch = useDispatch()
-    const {id} = useParams()
-    const singleProduct = useSelector((state) => {
-        return state.singleProductSlice.singleProduct
-    })
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const singleProduct = useSelector((state) => {
+    return state.singleProductSlice.singleProduct;
+  });
 
-    const sessionId = localStorage.getItem('session_id')
+  const sessionId = localStorage.getItem("session_id");
 
-    useEffect(() => {
-        dispatch(fetchSingleProduct(id))
-    }, [dispatch, id])
+  useEffect(() => {
+    dispatch(fetchSingleProduct(id));
+  }, [dispatch, id]);
 
-    const addToCart = async (id) => {
-        try {
-            let cartId = await axios.post('/api/orders/cartadd', {
-                'productId': id,
-                'sessionId': sessionId 
-            })
-            cartId = cartId.data.cartId;
-            dispatch({
-                type: 'main/setCartId',
-                payload: cartId
-            })
-        } catch (err) {
-            if(err.response && err.response.data) {
-                console.log(err.response.data)
-            } else {console.log(err)}
-        }
+  const addToCart = async (id) => {
+    try {
+      let cartId = await axios.post("/api/orders/cartadd", {
+        productId: id,
+        sessionId: sessionId,
+      });
+      cartId = cartId.data.cartId;
+      dispatch({
+        type: "main/setCartId",
+        payload: cartId,
+      });
+    } catch (err) {
+      if (err.response && err.response.data) {
+        console.log(err.response.data);
+      } else {
+        console.log(err);
+      }
     }
+  };
 
-    return (
-        <div>
-            <img src={singleProduct.image_url}/>
-            <button onClick={()=>addToCart(singleProduct.id)}>Add To Cart</button>
-            <h1>{singleProduct.name}</h1>
-            <h1>{singleProduct.price}</h1>
-            <h2>{singleProduct.description}</h2>
-            <h3>{singleProduct.quantity}</h3> 
-        </div>
-    )
-}
+  return (
+    <div className="single-product-container">
+      <div className="single-product-image">
+        <img src={singleProduct.image_url} />
+      </div>
+      <div className="single-product-details">
+        <button
+          className="addcart-button"
+          onClick={() => addToCart(singleProduct.id)}
+        >
+          Add To Cart
+        </button>
+        <h1 className="product-name">{singleProduct.name}</h1>
+        <h1 className="product-price">{singleProduct.price}</h1>
+        <h2 className="product-description">{singleProduct.description}</h2>
+        <h3 className="product-quantity">
+          "Only {singleProduct.quantity} left in stock - Order soon"
+        </h3>
+      </div>
+    </div>
+  );
+};
 
-export default SingleProduct
+export default SingleProduct;
