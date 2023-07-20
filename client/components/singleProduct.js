6124fbf3ebbaad63a53cd6eacd542117fe9f4878
JordanSucher <jsucher@gmail.com>
2023-07-20 15:24:15 -0400

    Added add-to-cart functionality, FE and BE

--- client/components/singleProduct.js
+++ client/components/singleProduct.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { fetchSingleProduct } from "../store/singleProductSlice";
+import axios from "axios";
 
 const SingleProduct = () => {
     const dispatch = useDispatch()
@@ -10,13 +11,34 @@ const SingleProduct = () => {
         return state.singleProductSlice.singleProduct
     })
 
+    const sessionId = localStorage.getItem('session_id')
+
     useEffect(() => {
         dispatch(fetchSingleProduct(id))
     }, [dispatch, id])
 
+    const addToCart = async (id) => {
+        try {
+            let cartId = await axios.post('/api/orders/cartadd', {
+                'productId': id,
+                'sessionId': sessionId 
+            })
+            cartId = cartId.data.cartId;
+            dispatch({
+                type: 'main/setCartId',
+                payload: cartId
+            })
+        } catch (err) {
+            if(err.response && err.response.data) {
+                console.log(err.response.data)
+            } else {console.log(err)}
+        }
+    }
+
     return (
         <div>
             <img src={singleProduct.image_url}/>
+            <button onClick={()=>addToCart(singleProduct.id)}>Add To Cart</button>
             <h1>{singleProduct.name}</h1>
             <h1>{singleProduct.price}</h1>
             <h2>{singleProduct.description}</h2>
