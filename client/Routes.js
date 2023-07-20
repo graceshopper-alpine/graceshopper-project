ef445849ec046d31495560295bb195dc07743ec4
JoeLomuto <lomutoj@gmail.com>
2023-07-20 18:19:57 -0400

    lower s to cap s

--- client/Routes.js
+++ client/Routes.js
@@ -5,7 +5,7 @@ import { Login, Signup } from "./components/AuthForm";
 import Home from "./components/Home";
 import { me } from "./store";
 import Products from "./components/AllProducts";
-import SingleProduct from "./components/singleProduct";
+import SingleProduct from "./components/SingleProduct";
 
 /**
  * COMPONENT
