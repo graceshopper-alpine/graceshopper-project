365b80d71a44838fa685d77e6500aeeca6b48b62
JoeLomuto <lomutoj@gmail.com>
2023-07-20 14:27:48 -0400

    single route

--- client/Routes.js
+++ client/Routes.js
@@ -23,7 +23,7 @@ class Routes extends Component {
         {isLoggedIn ? (
           <Switch>
             <Route path="/home" component={Home} />
-            <Route path="/singleProducts/:id" element={<SingleProduct />} />
+            <Route path="/products/:id" element={<SingleProduct />} />
             <Redirect to="/home" />
           </Switch>
         ) : (
