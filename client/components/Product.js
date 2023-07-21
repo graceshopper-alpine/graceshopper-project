3666a76c7a6e83a226de68512e790db688a10896
JordanSucher <jsucher@gmail.com>
2023-07-20 22:59:15 -0400

    Added more styling

--- client/components/Product.js
+++ client/components/Product.js
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 const Product = ({ product, color }) => {
   return (
     <Link to={`/products/${product.id}`} >
-      <section className="product-list-container" onMouseOver={(e) => e.currentTarget.style.backgroundColor = color} onMouseOut={(e) => e.currentTarget.style.backgroundColor = '#EDEDED'}>
+      <section className="product-list-container" onMouseOver={(e) => {e.currentTarget.style.backgroundColor = '#FFD4A1'}} onMouseOut={(e) => e.currentTarget.style.backgroundColor = '#EDEDED'}>
         
           <img src={`https://picsum.photos/seed/${product.id}/200`} />
           <span className="product-list-item-details">
-            <h3>{product.name}</h3>
-            <h3>{product.price}</h3>
+            <h3 name='product'>{product.name.toUpperCase()}</h3>
+            <h3 name='price'>{product.price}</h3>
           </span>
       </section>
     </Link>
