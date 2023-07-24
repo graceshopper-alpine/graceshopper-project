afd213fd2f8cbb1e4b62acc5d40a687f0ceac90d
JoeLomuto <lomutoj@gmail.com>
2023-07-24 12:16:27 -0400

    moved some jsx around

--- client/components/Checkout.js
+++ client/components/Checkout.js
@@ -2,7 +2,6 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 const Checkout = () => {
-
   const cart = useSelector((state) => state.main.cart);
 
   return (
@@ -13,54 +12,50 @@ const Checkout = () => {
         </div>
       </div>
       <div className="">
-      <div>
         <div>
-          <label>First Name</label>
-          <input type="text" name="firstname"></input>
+          <div>
+            <label>First Name</label>
+            <input type="text" name="firstname"></input>
+          </div>
+        </div>
+        <div>
+          <div>
+            <label>Last Name</label>
+            <input type="text" name="lastname"></input>
+          </div>
+        </div>
+        <div>
+          <div>
+            <label>Email Address</label>
+            <input type="text" name="email"></input>
+          </div>
+        </div>
+        <div>
+          <div>
+            <label>Street Address</label>
+            <textarea rows="3"></textarea>
+          </div>
+        </div>
+        <div>
+          <div>
+            <label>City</label>
+            <input type="text" name="city"></input>
+          </div>
+        </div>
+        <div>
+          <div>
+            <label>State</label>
+            <input type="text" name="state"></input>
+          </div>
+        </div>
+        <div>
+          <div>
+            <label>Zip Code</label>
+            <input type="text" name="zipcode"></input>
+          </div>
         </div>
       </div>
-      <div>
-        <div>
-          <label>Last Name</label>
-          <input type="text" name="lastname"></input>
-        </div>
-      </div>
-      <div>
-        <div>
-          <label>Email Address</label>
-          <input type="text" name="email"></input>
-        </div>
-      </div>
-      <div>
-        <div>
-          <label>Full Address</label>
-          <textarea rows="3"></textarea>
-        </div>
-      </div>
-      <div>
-        <div>
-          <label>City</label>
-          <input type="text" name="city"></input>
-        </div>
-      </div>
-      <div>
-        <div>
-          <label>State</label>
-          <input type="text" name="state"></input>
-        </div>
-      </div>
-      <div>
-        <div>
-          <label>Zip Code</label>
-          <input type="text" name="zipcode"></input>
-        </div>
-      </div>
-      </div>
-      <div>
-        <div>
-          <button type="button">Place Order</button>
-        </div>
-      </div>
+      <div></div>
 
       <div className="">
         <table>
@@ -74,10 +69,13 @@ const Checkout = () => {
           </thead>
           <tbody>
             <tr>
-                <td>{cart.Product}</td>
-                <td>{cart.Product}</td>
-                <td>{cart.quantity}</td>
-                <td>{cart.total}</td>
+              <td>{cart.Product}</td>
+              <td>{cart.Product}</td>
+              <td>{cart.quantity}</td>
+              <td>{cart.total}</td>
+              <div>
+                <button type="button">Place Order</button>
+              </div>
             </tr>
           </tbody>
         </table>
